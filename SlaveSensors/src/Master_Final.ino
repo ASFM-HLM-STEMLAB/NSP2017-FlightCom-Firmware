5541470faca9be0606660d32c5fdb38e5ed118bc
Rodrigo Chousal <rodrigochousal@gmail.com>
2018-05-07 16:18:43 -0500

    New master.

--- SlaveSensors/src/Master_Final.ino
+++ SlaveSensors/src/Master_Final.ino
@@ -1,23 +1,26 @@
+
 /*
  * Project HLM_I2C_MASTER
  * Description: HLM1 Master Module for I2C Slaves
  * Author: ASFM HLM STEM LAB - 2017 Near Space Team
  * License: MIT Open Source
- * Date: January/2018
+ * Date: May/2018
  */
-
+ 
 #include <Wire.h>
+
+int slaveCount = 0;
 
 // TMP36_Temperature STORAGE
 char tempTMP36[10]={}; // empty array for temperature
 
-// MS5607_Altimeter STORAGE
+// MS5607 STORAGE
 char tempMS5607[10]={}; // empty array for temperature
 char presMS5607[10]={}; // empty array for pressure
 
 // DHT22_Humidity STORAGE
-char humDHT22[10]={}; // empty array for humidity
 char tempDHT22[10]={}; // empty array for temperature
+char humDHT22[10]={}; // empty array for humidity
 
 int valueRequest = 0; // 0: TMP36-TEMPERATURE, 
                       // 1: MS5607-TEMPERATURE,
@@ -28,13 +31,22 @@ int valueRequest = 0; // 0: TMP36-TEMPERATURE,
 int maxValue = 4;
 
 void setup() {
+  
   // Start the I2C Bus as Master
   Wire.begin(); 
   Serial.begin(9600);
+  
+  Serial.println("\nI2C Scanner");
+  while (slaveCount < 1) {
+    scanI2C();
+  }
+  
 }
 
 void loop() {
 
+  Serial.println("CURRENT REQUEST: " + String(valueRequest));
+  
   // alternate between sensors
   Wire.beginTransmission(1); // transmit to Teensy on LINE 1
   Wire.write(valueRequest);
@@ -44,9 +56,9 @@ void loop() {
     
     // request TMP36-TEMPERATURE
     requestTeensy(valueRequest, tempTMP36, "Temperature [ºC]");
-  
+    
   } else if (valueRequest == 1) { // MS5607-TEMPERATURE
-
+    
     // request MS5607-TEMPERATURE
     requestTeensy(valueRequest, tempMS5607, "Temperature [ºC]");
     
@@ -67,39 +79,84 @@ void loop() {
     
   }
 
-  delay(200); //give some time to relax
+  valueRequest++;
+
+  if (valueRequest == maxValue + 1) {
+    
+    valueRequest = 0; // if last sensor, reset request
+
+  }
   
+  delay(1000); //give some time to relax
   
 }
 
 void requestTeensy(int value, char store[10], String unit) {
 
-   if (valueRequest <= maxValue) {
-      Wire.requestFrom(1, 5);    // request 5 bytes from Teensy (standard)
+  Wire.requestFrom(1, 5);    // request 5 bytes from Teensy (standard)
 
-      // gather data coming from Teensy
-      int i = 0; // counter for each byte as it arrives
-      while (Wire.available()) { 
-        store[i] = Wire.read(); // every character that arrives is written to empty array 'store'
-        i = i + 1;
-      }
+  // gather data coming from Teensy
+  int i = 0; // counter for each byte as it arrives
+  while (Wire.available()) { 
+     store[i] = Wire.read(); // every character that arrives is written to empty array 'store'
+     i = i + 1;
+  }
     
-      // better method for printing with a decimal point after 2 values would be nice
-      Serial.print(unit + ": ");
-      Serial.println(String(store[0]) 
-                      + String(store[1]) + "." 
-                      + String(store[2]) 
-                      + String(store[3]) 
-                      + String(store[4]));   //shows the data in the array 'store'
-      Serial.println("--------------");
+  // better method for printing with a decimal point after 2 values would be nice
+  Serial.print(unit + ": ");
+  Serial.println(String(store[0]) + 
+                 String(store[1]) + 
+                 String(store[2]) + 
+                 String(store[3]) + 
+                 String(store[4])); // shows the data in the array 'store'
+  Serial.println("--------------");
 
-      valueRequest += 1; // ask for next value next time
-      
-   } else {
-    
-      valueRequest = 0; // if greater than maxValue, reset
-   }
+}
+
+void scanI2C() {
   
-   
+  byte error, address;
+ 
+  Serial.println("Scanning...");
+ 
+  for(address = 1; address < 127; address++ ) {
+    
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0) {
+      
+      Serial.print("I2C device found at address 0x");
+      
+      if (address < 16) {
+        Serial.print("0");
+      }
+      
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      slaveCount++;
+      
+    } else if (error == 4) {
+      
+      Serial.print("Unknown error at address 0x");
+      
+      if (address < 16) {
+        Serial.print("0");
+        Serial.println(address,HEX);
+      }
+    }    
+  }
+  
+  if (slaveCount == 0) {
+    Serial.println("No I2C devices found\n");
+  } else {
+    Serial.println("done\n");
+  }
+
+  delay(5000);           // wait 5 seconds for next scan
 }
 
