1ec585d08c8b36a54ecae46af4a2a0ad942f6c2c
Rodrigo Chousal <rodrigochousal@gmail.com>
2018-05-22 23:23:27 -0500

    Final master specs for launch

--- SlaveSensors/src/Master_Final.ino
+++ SlaveSensors/src/Master_Final.ino
@@ -1,4 +1,3 @@
-
 /*
  * Project HLM_I2C_MASTER
  * Description: HLM1 Master Module for I2C Slaves
@@ -8,25 +7,30 @@
  */
  
 #include <Wire.h>
+#include "intersemaBaro.h"
 
 int slaveCount = 0;
 
 // TMP36_Temperature STORAGE
 char tempTMP36[10]={}; // empty array for temperature
 
-// MS5607 STORAGE
-char tempMS5607[10]={}; // empty array for temperature
-char presMS5607[10]={}; // empty array for pressure
-
 // DHT22_Humidity STORAGE
 char tempDHT22[10]={}; // empty array for temperature
 char humDHT22[10]={}; // empty array for humidity
 
+// ALTIMETER MODULE: MS5607
+Intersema::BaroPressure_MS5607B baro(true);
+
+// MS5607 STORAGE
+float decTempMS5607 = 0.0;
+float decPresMS5607 = 0.0;
+
 int valueRequest = 0; // 0: TMP36-TEMPERATURE, 
-                      // 1: MS5607-TEMPERATURE,
-                      // 2: MS5607-PRESSURE,
-                      // 3: DHT22-TEMPERATURE,
-                      // 4: DHT22-HUMIDITY
+                      // 1: DHT22-TEMPERATURE,
+                      // 2: DHT22-HUMIDITY,
+                      // 3: MS5607-TEMPERATURE,
+                      // 4: MS5607-PRESSURE
+    
 
 int maxValue = 4;
 
@@ -35,12 +39,10 @@ void setup() {
   // Start the I2C Bus as Master
   Wire.begin(); 
   Serial.begin(9600);
-  
-  Serial.println("\nI2C Scanner");
-  while (slaveCount < 1) {
-    scanI2C();
-  }
-  
+
+  setupMS5607();
+
+  Serial.println("SETUP DONE");
 }
 
 void loop() {
@@ -57,26 +59,25 @@ void loop() {
     // request TMP36-TEMPERATURE
     requestTeensy(valueRequest, tempTMP36, "Temperature [ºC]");
     
-  } else if (valueRequest == 1) { // MS5607-TEMPERATURE
-    
-    // request MS5607-TEMPERATURE
-    requestTeensy(valueRequest, tempMS5607, "Temperature [ºC]");
-    
-  } else if (valueRequest == 2) { // MS5607-PRESSURE
-
-    // request MS5607-PRESSURE
-    requestTeensy(valueRequest, presMS5607, "Pressure [Pa]");
-    
-  } else if (valueRequest == 3) { // DHT22-TEMPERATURE
+  } else if (valueRequest == 1) { // DHT22-TEMPERATURE
     
     // request DHT22-TEMPERATURE
     requestTeensy(valueRequest, tempDHT22, "Temperature [ºC]");
     
-  } else if (valueRequest == 4) { // DHT22-HUMIDITY
+  } else if (valueRequest == 2) { // DHT22-HUMIDITY
 
     // request DHT22-HUMIDITY
     requestTeensy(valueRequest, humDHT22, "Relative Humidity [%]");
     
+  } else if (valueRequest == 3) { // MS5607-TEMPERATURE
+    
+    // request MS5607-TEMPERATURE
+    decTempMS5607 = readMS5607("t");
+      
+  } else if (valueRequest == 4) { // MS5607-PRESSURE
+
+    // request MS5607-PRESSURE
+    decPresMS5607 = readMS5607("p");
   }
 
   valueRequest++;
@@ -101,6 +102,12 @@ void requestTeensy(int value, char store[10], String unit) {
      store[i] = Wire.read(); // every character that arrives is written to empty array 'store'
      i = i + 1;
   }
+
+  if (value == 0) {
+    Serial.println("TMP36");
+  } else if (value < 3) {
+    Serial.println("DHT22");
+  }
     
   // better method for printing with a decimal point after 2 values would be nice
   Serial.print(unit + ": ");
@@ -113,50 +120,55 @@ void requestTeensy(int value, char store[10], String unit) {
 
 }
 
-void scanI2C() {
-  
-  byte error, address;
- 
-  Serial.println("Scanning...");
- 
-  for(address = 1; address < 127; address++ ) {
-    
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0) {
-      
-      Serial.print("I2C device found at address 0x");
-      
-      if (address < 16) {
-        Serial.print("0");
-      }
-      
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      slaveCount++;
-      
-    } else if (error == 4) {
-      
-      Serial.print("Unknown error at address 0x");
-      
-      if (address < 16) {
-        Serial.print("0");
-        Serial.println(address,HEX);
-      }
-    }    
-  }
-  
-  if (slaveCount == 0) {
-    Serial.println("No I2C devices found\n");
-  } else {
-    Serial.println("done\n");
-  }
+float readMS5607(String value) {
 
-  delay(5000);           // wait 5 seconds for next scan
+  Serial.println("MS5607");
+
+  if (value == "t") {
+    decTempMS5607 = baro.getTemperature();
+    Serial.print("Temperature [ºC]: ");
+    Serial.println(decTempMS5607);
+    Serial.println("--------------");
+    
+  } else {
+    decPresMS5607 = baro.getPressurePascals();
+    Serial.print("Pressure [Pa]: ");
+    Serial.println(decPresMS5607);
+    Serial.println("--------------");
+  }
 }
 
+void setupMS5607() {
+
+  baro.init();
+
+  Serial.println("------------------------------------");
+
+  Serial.println("SETTING UP MS5607");
+  Serial.println("."); 
+  Serial.println("."); 
+  Serial.println(".");
+
+  long alt = baro.getHeightCentiMeters();
+  Serial.println("Got altitude");
+  
+  long pre = baro.getPressurePascals();
+  Serial.println("Got pressure");
+  
+  long temp = baro.getTemperature();
+  Serial.println("Got temperature");
+
+  Serial.println("--ALTITUDE--");
+  Serial.println("Centimeters:");
+  Serial.println((float)(alt), 2);
+
+  Serial.println("--PRESSURE--");
+  Serial.println("Pressure (Pa): ");
+  Serial.println((float)(pre), 2);
+
+  Serial.println("--TEMPERATURE--");
+  Serial.println("Temperature (C):");
+  Serial.print((float)(temp / 100), 2);
+  
+  Serial.println("------------------------------------");
+}
